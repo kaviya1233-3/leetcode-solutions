0e1cfc901f608555ad852f631ef5c88284c83a1f
kaviya1233-3 <kaviyathiagarajan2006@gmail.com>
2026-03-18 11:34:00 +0530

    Create valid palindrome.py
    added valid palindrome(two pointer)

---
+++ Two pointer/valid palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.casefold()
+        s=re.sub(r'[^a-zA-Z0-9]', '',s)
+        if s == s[::-1]:
+            return True
+        return False
+        
